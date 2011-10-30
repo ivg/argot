1fce75e3477ea7c69ce90353ccd17886de616922
argot <argot@x9c.fr>
2011-10-30 10:38:29 -0400

    MINOR: 'Utils' module with string and i/o functions.
    Ignore-this: 34d67a7908b95a9532d27245160ff9d
    
    darcs-hash:20111030143829-8a6d7-d8a29fa60736c1c03b09a6a706722c41e0c09cf5

---
+++ src/utils.ml
@@ -0,0 +1,77 @@
+(*
+ * This file is part of Argot.
+ * Copyright (C) 2010-2011 Xavier Clerc.
+ *
+ * Argot is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Argot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+(* String functions *)
+
+let is_whitespace = function
+  | ' ' | '\t' | '\r' | '\n' -> true
+  | _ -> false
+
+let trim_gen left right s =
+  let i = ref 0 in
+  let len = String.length s in
+  if left then
+    while (!i < len) && (s.[!i] = ' ') do
+      incr i
+    done;
+  let j = ref (pred len) in
+  if right then
+    while (!j >= !i) && (s.[!j] = ' ') do
+      decr j
+    done;
+  if j >= i then
+    String.sub s !i (!j - !i + 1)
+  else
+    ""
+
+let trim_left = trim_gen true false
+
+let trim_right = trim_gen false true
+
+let trim = trim_gen true true
+
+
+(* I/O functions *)
+
+let read_lines filename =
+  let chan = open_in filename in
+  let lines = ref [] in
+  try
+    while true do
+      lines := (input_line chan) :: !lines
+    done;
+    assert false
+  with
+  | End_of_file ->
+      close_in_noerr chan;
+      List.rev !lines
+  | e ->
+      close_in_noerr chan;
+      raise e
+
+let write_lines filename lines =
+  let chan = open_out filename in
+  try
+    List.iter
+      (fun line ->
+        output_string chan line;
+        output_char chan '\n')
+      lines
+  with e ->
+    close_out_noerr chan;
+    raise e
---
+++ src/utils.mli
@@ -0,0 +1,43 @@
+(*
+ * This file is part of Argot.
+ * Copyright (C) 2010-2011 Xavier Clerc.
+ *
+ * Argot is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Argot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+(** Miscellaneous utility functions. *)
+
+
+(** {6 String functions} *)
+
+val trim_left : string -> string
+(** Returns a copy of the string without leading whitespace. *)
+
+val trim_right : string -> string
+(** Returns a copy of the string without trailing whitespace. *)
+
+val trim : string -> string
+(** Returns a copy of the string without leading and trailing whitespace. *)
+
+
+(** {6 I/O functions} *)
+
+val read_lines : string -> string list
+(** [read_lines filename] returns the lines from the file named [filename].
+    Raises an exception if an i/o error occurs. *)
+
+val write_lines : string -> string list -> unit
+(** [write_lines filename lines] writes the lines from [lines] to the file
+    named [filename].
+    Raises an exception if an i/o error occurs. *)
